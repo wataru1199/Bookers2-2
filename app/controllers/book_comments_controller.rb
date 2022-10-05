4e7b934ef46beb37167d8280c7890a6cd686e903
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-10-05 14:19:34 +0000

    [Update6]10/5

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,7 +5,14 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back fallback_location: root_path
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+
+  end
+
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
   end
 
   private

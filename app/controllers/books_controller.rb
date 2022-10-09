f44e253a4adf0708c2811353b86238a726f780b7
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-10-09 14:34:32 +0000

    [Update7a]10/9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,13 @@ before_action :ensure_correct_user, only: [:update, :edit]
   end
 
   def index
-    @books = Book.all
-    @books = Book.includes(:favorites).sort {|a,b| b.favorites.size <=> a.favorites.size}
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+    sort {|a,b|
+        b.favorites.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorites.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
   end
 
